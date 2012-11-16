3c7d9a0b50e6f94bc47ee167ab295b2b3552bd5e
Tim Vergenz <vergenzt@gmail.com>
2012-11-16 17:15:28 -0500

    Debug matrixmult

--- matrixmult.py
+++ matrixmult.py
@@ -9,12 +9,15 @@ def get_input():
 
         yield (n, a)
 
+# define irange to be an inclusive range function
+irange = lambda l,u: xrange(l,u+1)
+
 def solve(n, a):
     C = [[0]*n]*n
-    for k in xrange(2,n+1):
-        for i in xrange(0, n-k+1)):
-            C[i][i+k-1] = min(C[i][j] + C[j+1][i+k-1] + a[i]*a[j+1]*a[i+k] for j in xrange(i, i+k-2))
-    return C[0][n-1]
+    for k in irange(2,n):
+        for i in irange(0, n-k):
+            C[i][i+k-1] = min(C[i][j] + C[j+1][i+k-1] + a[i]*a[j+1]*a[i+k-1] for j in irange(i, i+k-2))
+    print C[0][n-1]
 
 for case in get_input():
     solve(*case)
